ce1b7326edf4f526b6c8bb9920fc2ee6490a7d87
Matthew Nessworthy <matthewn@ringier.co.za>
2021-08-03 13:21:13 +0200

    sdc_search_config

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,34 @@
 <?php
+
+use Ringierimu\Experiments\Facades\SdcExperiments;
+
+if (!function_exists('sdc_search_config')) {
+    /**
+     * Get the SDC Search config merged from regular config and experiment config
+     *
+     * @param string $key
+     * @param mixed $default
+     *
+     * @return mixed
+     */
+    function sdc_search_config(
+        string $key,
+        $default = null
+    ) {
+        $testGroup = SdcExperiments::getOrStartExperiment('sdc_search') ?: 'control';
+
+        $testGroupConfigKey = sprintf(
+            'experiments.tests.sdc_search.%s',
+            $testGroup
+        );
+
+        return data_get(
+            array_replace_recursive(
+                config('sdc_search') ?: [],
+                config($testGroupConfigKey) ?: []
+            ),
+            $key,
+            $default
+        );
+    }
+}
